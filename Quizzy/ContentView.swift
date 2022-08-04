c7f782ef6a180a5b6f3d35a92e57e656b5fdf658
Ali Feras <ferasali876@gmail.com>
2022-08-04 10:46:39 -0500

    Initial Commit

---
+++ Quizzy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quizzy
+//
+//  Created by Ali on 04/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quizzy/QuizzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzyApp.swift
+//  Quizzy
+//
+//  Created by Ali on 04/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct QuizzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
